99f64fc57cd0d6776aa6d356fab68e050f8bbc92
franksen <youngsenman@163.com>
2021-08-13 23:04:20 +0800

    [add JenkinsJavaBean] by franksen.

---
+++ src/main/java/JenkinsJavaBean.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: Franksen
+ * @Program: PACKAGE_NAME
+ * @Date: 2021/8/13 22:53
+ * @Description:
+ */
+public class JenkinsJavaBean {
+
+    public static void main(String[] args) {
+        System.out.println("hello jenkins");
+    }
+}
